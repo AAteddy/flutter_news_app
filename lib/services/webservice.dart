0fbfe26ffc93ecb45d3d9e341e8f2d7a4c7fc257
Tedros Alem <tedsaasfaha@gmail.com>
2023-02-02 00:11:28 +0300

    created webservice.dart file inside services directory

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -1,8 +1,20 @@
+import 'dart:convert';
+import 'package:flutter_news_app/models/newsArticle.dart';
 import 'package:http/http.dart' as http;
 
 class Webservice {
-  void fetchTopHeadlines() {
+  Future<List<NewsArticle>> fetchTopHeadlines() async {
     String url =
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=28463e4260e24672ad1f981f9c02e1fb";
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final result = jsonDecode(response.body);
+      Iterable list = result["articles"];
+      return list.map((article) => NewsArticle.fromJSON(article)).toList();
+    } else {
+      throw Exception("Failed to get top news");
+    }
   }
 }
